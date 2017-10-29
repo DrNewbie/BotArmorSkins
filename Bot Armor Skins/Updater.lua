883000fecc9088bb80d73c31328012b4ebbcd588
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-30 01:35:24 +0800

    back2old

--- Bot Armor Skins/Updater.lua
+++ Bot Armor Skins/Updater.lua
@@ -2,7 +2,7 @@ if UpdateThisMod then
 	UpdateThisMod:Add({
 		mod_id = 'Bot Armor Skins',
 		data = {
-			modworkshop_id = 'BotArmorSkins_20918',
+			modworkshop_id = 20918,
 			dl_url = 'https://github.com/DrNewbie/BotArmorSkins/raw/master/Bot%20Armor%20Skins.zip',
 			info_url = 'https://raw.githubusercontent.com/DrNewbie/BotArmorSkins/master/Bot%20Armor%20Skins/mod.txt'
 		}
