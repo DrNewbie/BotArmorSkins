b0e04bb1dd1d24eca5b5d5873cd3c55dd053c15b
Dr_Newbie <tf2baidonation@gmail.com>
2018-12-04 21:28:12 +0800

    18

--- Bot Armor Skins/lua/teamaibase.lua
+++
@@ -1,5 +0,0 @@
-require("lib/units/ArmorSkinExt")
-
-Hooks:PostHook(TeamAIBase, "post_init", "BotArmorSkins_TeamAIBase_post_init", function(self)
-	self._bot_armor_skin = ArmorSkinExt:new(self._unit)
-end)
